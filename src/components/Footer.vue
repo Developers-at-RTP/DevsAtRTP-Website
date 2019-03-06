0136a3274d4fdc2216aae040b8781f066cedb8e0
Maria Lamardo <maria.lamardo@nutanix.com>
2019-03-05 21:03:03 -0500

    Links / content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,17 @@
 <template>
   <footer class="footer">
     <div custom-layout="4 4 4">
-      <span>Twitter</span>
+      <a href="https://twitter.com/DevelopersAtRTP" class="socialMedia">
+        <img src="@/assets/twitter.svg" alt="Twitter" class="socialMediaIcon">
+      </a>
       <h1>DEVS@RTP</h1>
-      <span>Meetup</span>
+      <a href="https://www.meetup.com/Developers-RTP/" class="socialMedia">
+        <img src="@/assets/meetup.svg" alt="Meetup" class="socialMediaIcon">
+      </a>
     </div>
   </footer>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home paddingTop paddingBottom">
     <img src="@/assets/D@RTimg.jpg" alt="Group of developers" class="dartImg">
+    <main class="mainContent">
+      <h1>Who is Devs@RTP?</h1>
+      <p>We are a warm, imaginative and energetic group of developers. We hold weekly study sessions, host technical Meetup events, attend technical events and conferences as a group as well as plan other non-technical social events.</p>
+    </main>
+    <aside></aside>
   </div>
 </template>
 
